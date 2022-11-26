15c019224085d5df0cef197a8fc066e0285b11ca
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-11-26 09:11:57 +0700

    /add task 2

---
+++ task_2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число");
+int a = Convert.ToInt32(Console.ReadLine()); // Ввод чисел
+Console.Write("Введите 2-ое  число");
+int b = Convert.ToInt32(Console.ReadLine()); // Ввод чисел
+    if (a > b) 
+{
+    Console.Write("a=max");
+}      
