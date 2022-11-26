2d299e2dff87a7b0b97387dcb3aad014c0d28a01
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-11-26 11:02:11 +0700

    /add task 4

---
+++ task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число");
+int a = Convert.ToInt32(Console.ReadLine()); // Ввод чисел
+Console.Write("Введите 2-ое  число");
+int b = Convert.ToInt32(Console.ReadLine()); // Ввод чисел
+Console.Write("Введите 1-ое число");
+int c = Convert.ToInt32(Console.ReadLine()); // Ввод чисел
+if (a > b && a > c)
+    Console.Write(a);
+if (b > a && b > c)
+    Console.Write(b);
+if (c > a && c > b)
+    Console.Write(c);
