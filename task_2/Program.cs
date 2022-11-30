9c6be0382519f2e3f36f7a79c1b29af0e2f495c5
OksanaMiheenko1412 <118578118+OksanaMiheenko1412@users.noreply.github.com>
2022-11-30 23:05:57 +0700

    / add commit task2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Clear();
+﻿// Задача 2. Программа на вход принимает 2 числа и выдает какое больше, а какое
+// мельше. Например: a=5,b=7 ->max =7; a=-9,b=-3 ->max=-3
+Console.Clear();
 Console.Write("Введите 1-ое число");
 int a = Convert.ToInt32(Console.ReadLine()); // Ввод чисел
 Console.Write("Введите 2-ое  число");
